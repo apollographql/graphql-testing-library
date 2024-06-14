56499d9885ba2be302a3b215846ba63c2070130e
Alessia Bellisario <github@bellisar.io>
2024-06-14 15:15:36 -0400

    Move things around

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,32 +2,18 @@ import type { Preview } from "@storybook/react";
 import { initialize, mswLoader, getWorker } from "msw-storybook-addon";
 import "./stories/input.css";
 
-let options = {};
-
-if (location.hostname === "apollographql.github.io") {
-  options = {
-    serviceWorker: {
-      url: "/graphql-testing-library/mockServiceWorker.js",
-    },
-  };
-}
-
 // Initialize MSW
-initialize(options);
+initialize({
+  serviceWorker: {
+    url: './mockServiceWorker.js'
+  }
+});
 
 const preview: Preview = {
   // calling getWorker().start() is a workaround for an issue
   // where Storybook doesn't wait for MSW before running:
   // https://github.com/mswjs/msw-storybook-addon/issues/89
   loaders: [mswLoader, () => getWorker().start()],
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
 };
 
 export default preview;
