0c798fc96b2d72700b43433b534c56aa1310cadd
Alessia Bellisario <github@bellisar.io>
2024-06-14 13:14:26 -0400

    Add docs pages

--- .storybook/stories/components/Container.tsx
+++ .storybook/stories/components/Container.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from "react";
 function Container({ children }: { children: ReactNode }) {
   return (
     <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <div className="mx-auto max-w-2xl px-4 py-4 sm:px-6 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">
           Customers also purchased
         </h2>
