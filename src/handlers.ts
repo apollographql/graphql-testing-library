c0382f374f41e8fdb3a0fd6c0f4bc15e273fc66c
Alessia Bellisario <github@bellisar.io>
2024-07-15 16:37:06 -0400

    delay of 2

--- src/handlers.ts
+++ src/handlers.ts
@@ -34,7 +34,7 @@ interface Options {
 
 export const createHandler = (
   schema: GraphQLSchema,
-  { delay }: Options = { delay: 1 }
+  { delay }: Options = { delay: 2 }
 ) => {
   // console.time("handler");
   let testSchema: GraphQLSchema = schema;
